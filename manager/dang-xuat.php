0e31f896e380605772061b05ee50d4279b368bae
‘SuaNguyen2004’ <abc@gmail.com>
2026-05-06 09:40:45 +0700

    dang-xuat

--- manager/dang-xuat.php
+++ manager/dang-xuat.php
@@ -1,27 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-// Xóa toàn bộ biến session
-$_SESSION = array();
-
-// Nếu muốn xóa cả Cookie session (an toàn tuyệt đối)
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httponly"]
-    );
-}
-
-// Hủy session
+session_start();
+session_unset();
 session_destroy();
-
-// Chuyển hướng về trang chủ
-header("Location: ../index.php");
-exit();
+header('location: ../index.php');
